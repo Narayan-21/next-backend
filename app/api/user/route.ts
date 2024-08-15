b467c26d8bf6e04c2cc30f69f0b403ec577f4a25
Narayan-21 <naaidjan.19@gmail.com>
2024-08-16 02:30:33 +0530

    prisma addition

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,6 +1,27 @@
-export function GET() {
-    return Response.json({
-        email: 'narine@gmail.com',
-        name: 'narine'
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from '@prisma/client';
+
+const client = new PrismaClient();
+
+export async function POST(req: NextRequest) {
+    const body = await req.json();
+    console.log(body)
+
+    await client.user.create({
+        data: {
+            username: body.username,
+            password: body.password
+        }
+    })
+
+    return NextResponse.json({
+        message: "you are logged in!"
+    })
+}
+
+export function GET(req: NextRequest) {
+    return NextResponse.json({
+        name: 'narine',
+        email: 'narine420@gmail.com'
     })
 }
